8b098d619d639553f53b5a6f8b134ced9e4e0c72
JIYOUNG <0803jiyoung99@gmail.com>
2024-03-08 12:34:27 +0900

    Refactor: types 컴포넌트 분리

--- src/components/types/Todo.tsx
+++ src/components/types/Todo.tsx
@@ -4,3 +4,24 @@ export type Todo = {
   content: string;
   isDone: boolean;
 };
+
+export type TodoFormProps = {
+  title: string;
+  content: string;
+  onChangeTitleHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeContentHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  clickAddTodoButton: (event: React.FormEvent) => void;
+};
+
+export type TodoListProps = {
+  headTitle: string;
+  todos: Todo[];
+  clickDeleteTodoButton: (id: string) => void;
+  clickUpdateTodoButton: (id: string) => void;
+};
+
+export type TodoItemProps = {
+  todo: Todo;
+  clickDeleteTodoButton: (id: string) => void;
+  clickUpdateTodoButton: (id: string) => void;
+};
